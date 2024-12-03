4ad6b5ea8b0bb116b3c24cdad65c7bc1b4e54d1a
hoigoysohio <njuli@yahoo.com>
2024-12-03 17:49:42 +0000

    Completed

---
+++ main.py
@@ -0,0 +1,10 @@
+text = input("Enter the text")
+print("Original text:", text)
+
+# reverse a string
+reversed_text = text [::-1]
+print("Reversed text:" , reversed_text)
+
+# How many digits in the 
+length = len(text)
+print ("length of the test:", length)
